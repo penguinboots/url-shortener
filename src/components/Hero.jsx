8cb3805f5506f6f912774e9078bd05593e323279
penguinboots <sabrina.ynw@gmail.com>
2023-05-29 14:03:06 -0700

    style Hero responsive behaviour

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,17 @@ import heroBanner from "../assets/illustration-working.svg";
 export default function Hero() {
   return (
     <div className="hero-wrapper">
-      <img className="hero-banner" src={heroBanner} />
-
       <div className="hero-text">
-        <h1>More than just shorter links</h1>
+        <h1>More than just</h1>
+        <h1>shorter links</h1>
         <p>
           Build your brand&#39;s recognition and get detailed insights on how
           your links are performing.
         </p>
         <button className="btn-main">Get Started</button>
       </div>
+
+      <img className="hero-banner" src={heroBanner} />
     </div>
   );
 }
