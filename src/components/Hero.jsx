d5fe1e7109b4b87c52346f484de4ff8a4f033e8e
penguinboots <sabrina.ynw@gmail.com>
2023-05-29 12:58:08 -0700

    add Hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,18 @@
+import heroBanner from "../assets/illustration-working.svg";
+
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="hero-wrapper">
+      <img className="hero-banner" src={heroBanner} />
+
+      <div className="hero-text">
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand&#39;s recognition and get detailed insights on how
+          your links are performing.
+        </p>
+        <button className="btn-main">Get Started</button>
+      </div>
+    </div>
+  );
 }
